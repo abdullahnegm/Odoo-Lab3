242f803c518dd40e094f1a7a99e49d0a82753731
abdullahnegm <abdullahahmed1997@hotmail.com>
2021-06-05 20:17:23 +0200

    Lab 3

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "HMS",
+    "summary": "Hospital Managment System",
+    "description": "Extends our final project: Otlob Momared",
+    "author": "Muhammad Taha, Ahmed Behiery, and Islam Ahmed",
+    "version": "1.0.0",
+    'data': [
+        "security/hms_groups.xml",
+        "security/ir.model.access.csv",
+        "views/hms_patient.xml",
+        "views/hms_departments.xml",
+        "views/hms_doctors.xml",
+        "views/hms_logs.xml",
+        "reports/report.xml",
+        "reports/hms_template.xml"
+    ]
+}
